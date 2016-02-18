3a154bf805d9f7b77e2c4597cfc97f3fd02cf8cb
Ajeet Goyal <ajeetgoyal1997@gmail.com>
2016-02-18 17:40:14 +0530

    Created login system

--- CS223/GMS/urls.py
+++ CS223/GMS/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'GMS'
 urlpatterns = [
 	url(r'^$', views.home, name = 'home'),
 	url(r'^login/', views.login, name = 'login'),
--- CS223/GMS/views.py
+++ CS223/GMS/views.py
@@ -1,13 +1,50 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.core.context_processors import csrf
+
+from .models import User
 
 def home(request):
-	return HttpResponse("Hello :P")
+	if "loggedinuserid" in request.session:
+		user = User.objects.get(userID = request.session["loggedinuserid"])
+		return render(request, 'GMS/home.html', {'user' : user})
+		# return HttpResponse("Hello, " + request.session["loggedinuserid"])
+	else:
+		return HttpResponseRedirect(reverse('GMS:login'))
 
 
 def login(request):
-	return HttpResponse("You'r seeing login page")
+	userid = request.POST.get('username', '')
+	pwd = request.POST.get('password', '')
+
+	c = {}
+	c.update(csrf(request))
+
+	if "loggedinuserid" in request.session:
+		return HttpResponseRedirect(reverse('GMS:home'))
+
+	if userid == '':
+		return render_to_response('GMS/login.html', c)
+
+	try:
+		user = User.objects.get(userID = userid)
+	except(KeyError, User.DoesNotExist):
+		c.update({ 'error_message':'1. Incorrect username or password' })
+		return render_to_response('GMS/login.html', c)
+	else:
+		if pwd != user.password :
+			c.update({ 'error_message':'2. Incorrect username or password' })
+			return render_to_response('GMS/login.html', c)
+		else :
+			request.session["loggedinuserid"] = userid
+			return HttpResponseRedirect(reverse('GMS:home'))
 
 
 def logout(request):
-	return HttpResponse("You'r seeing logout page")
+	try:
+		del request.session["loggedinuserid"]
+	except(KeyError):
+		return HttpResponseRedirect(reverse('GMS:home'))
+	
+	return render(request, 'GMS/loggedout.html')
