59cbeb0e29097b243b99c8e8c3aee4319ca96e54
Mahak Jain <jain.1@iitj.ac.in>
2016-02-18 22:39:34 +0530

    Updated databases

--- CS223/GMS/admin.py
+++ CS223/GMS/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import User, Instructor, Student, Course
+from .models import *
 
 admin.site.register(User)
 admin.site.register(Instructor)
 admin.site.register(Student)
 admin.site.register(Course)
+admin.site.register(Courses_CSE)
+admin.site.register(Courses_ME)
+admin.site.register(Courses_EE)
--- CS223/GMS/models.py
+++ CS223/GMS/models.py
@@ -18,6 +18,9 @@ class User(models.Model):
 class Instructor(models.Model):
 	user_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 	department = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.user_id.name + " [" + self.user_id.userID + "]"
 	
 
 Branch=(
@@ -26,11 +29,28 @@ Branch=(
 	(3, 'ME')
 )
 
+Batch=(
+	('1', 'UG2012'),
+	('2', 'UG2013'),
+	('3', 'UG2014'),
+	('4','UG2015')
+)
+
+Semester=(
+	(1, '1'),
+	(2, '2'),
+	(3, '3'),
+	(4, '4'),
+	(5, '5'),
+	(6, '6'),
+	(7, '7'),
+	(8, '8')
+	)
 class Student(models.Model):
 	user_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 	branch = models.IntegerField(default =1, choices=Branch)
-	batch = models.CharField(max_length=6, default="UG2014")
-	semester = models.IntegerField(default=1)
+	batch = models.CharField(max_length=6,choices=Batch)
+	semester = models.IntegerField(default=1,choices= Semester)
 
 	def __str__(self):
 		return self.user_id.name + " [" + self.user_id.userID + "]"
@@ -41,9 +61,37 @@ CourseType=(
 	(0, 'Audit')
 )
 
+Credits=(
+	(0,'0'),
+	(3,'3'),
+	(4,'4'),
+	(5,'5')
+)
+
 class Course(models.Model):
 	instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
 	courseID = models.CharField(max_length=5)
+	name = models.CharField(max_length=50, default = 'Unspecified')
 	LTP = models.CharField(max_length =5)
-	credits = models.IntegerField(default=0)
+	credits = models.IntegerField(default=0,choices=Credits)
 	courseType = models.BooleanField(default='true',choices = CourseType)
+	def __str__(self):
+		return self.name + " [" + self.courseID + "]"
+
+class Courses_CSE(models.Model):
+	sem = models.IntegerField(choices=Semester,primary_key=True)
+	courses = models.ManyToManyField(Course)
+	def __str__(self):
+		return "Semester " + str(self.sem) +" Courses"
+
+class Courses_ME(models.Model):
+	sem = models.IntegerField(choices=Semester,primary_key=True)
+	courses = models.ManyToManyField(Course)
+	def __str__(self):
+		return "Semester " + str(self.sem) +" Courses"
+
+class Courses_EE(models.Model):
+	sem = models.IntegerField(choices=Semester,primary_key=True)
+	courses = models.ManyToManyField(Course)
+	def __str__(self):
+		return "Semester " + str(self.sem) +" Courses"
